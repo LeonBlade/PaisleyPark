ce00072656f2eaca1cfa7b4ece8158e7b2eceaa7
James Stine <leon.blade@gmail.com>
2019-04-06 22:21:23 -0400

    Add a numeric textbox.

---
+++ PaisleyPark/Common/NumericTextBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PaisleyPark.Common
+{
+    public class NumericTextBox : TextBox
+    {
+        private readonly Regex NumericRegex = new Regex("[^0-9]+");
+
+        protected override void OnPreviewTextInput(TextCompositionEventArgs e)
+        {
+            e.Handled = !IsNumeric(e.Text);
+        }
+
+        private bool IsNumeric(string text)
+        {
+            return !NumericRegex.IsMatch(text);
+        }
+    }
+}
