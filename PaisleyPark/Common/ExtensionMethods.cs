7a7715608c280b0b28a980e946d6e2182f6d75be
James Stine <leon.blade@gmail.com>
2019-04-07 17:24:31 -0400

    Create version extension method.

--- PaisleyPark/Common/ExtensionMethods.cs
+++ PaisleyPark/Common/ExtensionMethods.cs
@@ -6,5 +6,6 @@ namespace PaisleyPark.Common
 	{
 		public static string AsHex(this ulong s) => string.Format("0x{0:X}", s);
 		public static ulong ToUint64(this IntPtr ptr) => (ulong)ptr.ToInt64();
+        public static string VersionString(this Version v) => string.Format("v{0}.{1}.{2}", v.Major, v.Minor, v.Build);
 	}
 }
