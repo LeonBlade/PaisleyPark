88d0494a4317398f22dbed203298ae2b986430f3
James Stine <leon.blade@gmail.com>
2019-05-14 07:47:29 -0400

    Fix address for Patch 4.58

--- PaisleyPark/ViewModels/MainWindowViewModel.cs
+++ PaisleyPark/ViewModels/MainWindowViewModel.cs
@@ -288,7 +288,7 @@ namespace PaisleyPark.ViewModels
 				// Waymark function address.
 				// TODO: AoB!
 				// 48 89 6C 24 10 48 89 74 24 18 57 48 83 EC 30 8B EA 49 8B F0 48 8B F9 83 FA 06
-				var waymarkFunc = (ffxiv_dx11 + 0x752140).ToUint64();
+				var waymarkFunc = (ffxiv_dx11 + 0x7521F0).ToUint64();
 				// Waymark class instance. (?)
 				var waymarkClassPointer = (ffxiv_dx11 + 0x1AE57C0).ToUint64();
 
