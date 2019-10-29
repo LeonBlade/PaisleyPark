d142d62360d0d19ec9442d35bd71d28dc6a9483e
James Stine <leon.blade@gmail.com>
2019-10-29 13:08:22 -0400

    Add ActorTable to Offset model.

--- PaisleyPark/Models/Offsets.cs
+++ PaisleyPark/Models/Offsets.cs
@@ -1,9 +1,10 @@
-﻿namespace PaisleyPark.Models
+namespace PaisleyPark.Models
 {
     public class Offsets
     {
         public int WaymarkClassPtr { get; set; }
         public int WaymarkFunc { get; set; }
         public int Waymarks { get; set; }
+        public int ActorTable { get; set; }
     }
 }
